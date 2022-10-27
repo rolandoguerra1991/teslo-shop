22510acfb9acb44986cc57c89a8185633e05d1de
rolando guerra <rolandoguerra1991@gmail.com>
2022-10-26 21:20:21 -0400

    insertando productos en la base de datos

--- src/products/products.service.ts
+++ src/products/products.service.ts
@@ -1,11 +1,25 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, InternalServerErrorException } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
 import { CreateProductDto } from './dto/create-product.dto';
 import { UpdateProductDto } from './dto/update-product.dto';
+import { Product } from './entities/product.entity';
 
 @Injectable()
 export class ProductsService {
+  constructor(
+    @InjectRepository(Product)
+    private readonly productRepository: Repository<Product>,
+  ) {}
+
   async create(createProductDto: CreateProductDto) {
-    return 'This action adds a new product';
+    try {
+      const product = this.productRepository.create(createProductDto);
+      await this.productRepository.save(product);
+      return product;
+    } catch (error) {
+      throw new InternalServerErrorException('Help!!');
+    }
   }
 
   findAll() {
